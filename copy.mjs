c279b9364162ec1724248806b9c8593fa768a9b9
eetann <hideharu092@gmail.com>
2024-07-28 16:50:43 +0900

    build: 開発用のスクリプトを追加

---
+++ copy.mjs
@@ -0,0 +1,23 @@
+import { copyFile } from "node:fs/promises";
+
+const filesToCopy = ["main.js", "styles.css", "manifest.json"];
+const pluginPath = process.env.PLUGIN_PATH;
+
+async function copyFiles() {
+	if (!pluginPath) {
+		console.error("The PLUGIN_PATH environment variable is required");
+		return;
+	}
+	try {
+		for (const file of filesToCopy) {
+			const sourcePath = `./${file}`;
+			const destPath = `${pluginPath}/${file}`;
+			await copyFile(sourcePath, destPath);
+			console.log(`Copied ${file} to ${destPath}`);
+		}
+	} catch (err) {
+		console.error("Error copying files:", err);
+	}
+}
+
+copyFiles();
