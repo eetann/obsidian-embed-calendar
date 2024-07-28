1b5fe550e2d16d04898cabf380d9840634538cb2
eetann <hideharu092@gmail.com>
2024-07-28 19:46:32 +0900

    fix: Reactを有効化

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,25 @@
 import {
-	App,
-	Editor,
-	MarkdownView,
+	type App,
 	Modal,
 	Notice,
 	Plugin,
 	PluginSettingTab,
 	Setting,
 } from "obsidian";
+import { ReactRenderer } from "./render";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface EmbedCalendarSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: EmbedCalendarSettings = {
 	mySetting: "default",
 };
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class EmbedCalendar extends Plugin {
+	settings: EmbedCalendarSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -46,52 +45,16 @@ export default class MyPlugin extends Plugin {
 			id: "open-sample-modal-simple",
 			name: "Open sample modal (simple)",
 			callback: () => {
-				new SampleModal(this.app).open();
+				new EmbedCalendarModal(this.app).open();
 			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
+		this.registerMarkdownCodeBlockProcessor("aaa", (code, el, ctx) => {
+			ctx.addChild(new ReactRenderer(el));
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
-		);
+		this.addSettingTab(new EmbedCalendarSettingTab(this.app, this));
 	}
 
 	onunload() {}
@@ -105,11 +68,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
+class EmbedCalendarModal extends Modal {
 	onOpen() {
 		const { contentEl } = this;
 		contentEl.setText("Woah!");
@@ -121,10 +80,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class EmbedCalendarSettingTab extends PluginSettingTab {
+	plugin: EmbedCalendar;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: EmbedCalendar) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
