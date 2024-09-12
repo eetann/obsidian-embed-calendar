7889c93e41a306dc59e00eabfa2ed26c9df2372d
eetann <hideharu092@gmail.com>
2024-09-12 18:54:57 +0900

    refactor: 型を追加

--- src/usecase/getCodeBlockResultUseCase/getCodeBlockResultUseCase.ts
+++ src/usecase/getCodeBlockResultUseCase/getCodeBlockResultUseCase.ts
@@ -1,8 +1,14 @@
+import type { EventDTO } from "../event/eventDTO";
 import { CodeBlockLoader } from "./codeBlockLoader/codeBlockLoader";
 import { CodeBlockValidator } from "./codeBlockValidator/codeBlockValidator";
+import type { OptionsType } from "./codeBlockValidator/optionsValidator";
 
+export type CodeBlockResultType = {
+	options: OptionsType;
+	events: EventDTO[];
+};
 export class GetCodeBlockResultUseCase {
-	async execute(source: string) {
+	async execute(source: string): Promise<CodeBlockResultType> {
 		const codeBlock = await new CodeBlockLoader().execute(source);
 		return new CodeBlockValidator().execute(codeBlock);
 	}
