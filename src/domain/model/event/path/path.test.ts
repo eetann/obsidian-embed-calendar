d48d178e943e306e3a87883cc70efe2d33d15797
eetann <hideharu092@gmail.com>
2024-09-16 12:20:03 +0900

    feat: pathの拡張子判定を追加

--- src/domain/model/event/path/path.test.ts
+++ src/domain/model/event/path/path.test.ts
@@ -2,9 +2,12 @@ import { Path } from "./path";
 
 describe("Path", () => {
 	it("Expect correct conversion results for valid formats", () => {
-		expect(new Path("a").value).toBe("a");
+		expect(new Path("a.md").value).toBe("a.md");
 	});
-	it("should throw an error if the path is less than 1 letter long", () => {
-		expect(() => new Path("")).toThrow("Path must be at least 1 letter");
+	it("Error if the path is 1 letter", () => {
+		expect(() => new Path("")).toThrow("Path must be at least 4 letter");
+	});
+	it("Error if the path is not markdown", () => {
+		expect(() => new Path("foo.bar")).toThrow("Path must be markdown");
 	});
 });
--- src/domain/model/event/path/path.ts
+++ src/domain/model/event/path/path.ts
@@ -2,8 +2,11 @@ import { ValueObject } from "@/domain/model/shared/valueObject";
 
 export class Path extends ValueObject<string, "Path"> {
 	validate(path: string): void {
-		if (path.length < 1) {
-			throw new Error("Path must be at least 1 letter");
+		if (path.length < 4) {
+			throw new Error("Path must be at least 4 letter");
+		}
+		if (!path.endsWith(".md")) {
+			throw new Error("Path must be markdown");
 		}
 	}
 }
