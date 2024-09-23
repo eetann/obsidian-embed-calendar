8898ef858f083e7fa4c5ddae5e28bb8e6a716628
eetann <hideharu092@gmail.com>
2024-09-23 16:00:44 +0900

    style: リンク範囲をbox全体へ広げた

--- src/presentation/calendar/rbcCalendar/EventContent.tsx
+++ src/presentation/calendar/rbcCalendar/EventContent.tsx
@@ -4,7 +4,10 @@ import { tv } from "tailwind-variants";
 import { useDnDContext } from "../provider/DnDContextProvider";
 
 const style = tv({
-	base: "text-white no-underline block hover:text-white hover:no-underline",
+	base: [
+		"text-white no-underline hover:text-white hover:no-underline",
+		"block h-full", // To extend the link range to the entire box
+	],
 	variants: {
 		isDrag: { false: "internal-link" },
 	},
@@ -13,23 +16,21 @@ const style = tv({
 export default function EventContent({ event }: EventProps<EventDTO>) {
 	const { isDrag, setIsDrag } = useDnDContext();
 	return (
-		<span>
-			<a
-				target="_blank"
-				rel="noreferrer noopener"
-				className={style({ isDrag })}
-				data-href={event.path}
-				href={event.path}
-				onMouseUp={() => {
-					// 1. onMouseUp
-					// 2. setIsDrag(false)
-					// 3. "internal-link" makes this back to obsidian internal-link
-					// 4. href
-					setIsDrag(false);
-				}}
-			>
-				{event.title}
-			</a>
-		</span>
+		<a
+			target="_blank"
+			rel="noreferrer noopener"
+			className={style({ isDrag })}
+			data-href={event.path}
+			href={event.path}
+			onMouseUp={() => {
+				// 1. onMouseUp
+				// 2. setIsDrag(false)
+				// 3. "internal-link" makes this back to obsidian internal-link
+				// 4. href
+				setIsDrag(false);
+			}}
+		>
+			{event.title}
+		</a>
 	);
 }
