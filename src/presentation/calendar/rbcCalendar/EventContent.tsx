6bdd25b8f8fe4c6b72cf619b99d83d016b5cef8d
eetann <hideharu092@gmail.com>
2024-09-29 21:24:03 +0900

    style: Eventの文字色などを見やすく変更

--- src/presentation/calendar/rbcCalendar/EventContent.tsx
+++ src/presentation/calendar/rbcCalendar/EventContent.tsx
@@ -1,18 +1,7 @@
 import type { EventDTO } from "@/usecase/event/eventDTO";
 import parse from "html-react-parser";
 import type { EventProps } from "react-big-calendar";
-import { tv } from "tailwind-variants";
 import { useDnDContext } from "../provider/DnDContextProvider";
-
-const style = tv({
-	base: [
-		"text-white no-underline hover:text-white hover:no-underline",
-		"block h-full", // To extend the link range to the entire box
-	],
-	variants: {
-		isDrag: { false: "internal-link" },
-	},
-});
 
 export default function EventContent({ event }: EventProps<EventDTO>) {
 	const { isDrag, setIsDrag } = useDnDContext();
@@ -21,7 +10,15 @@ export default function EventContent({ event }: EventProps<EventDTO>) {
 			<a
 				target="_blank"
 				rel="noreferrer noopener"
-				className={style({ isDrag })}
+				className={isDrag ? "internal-link" : ""}
+				style={{
+					// To extend the link range to the entire box
+					display: "block",
+					height: "100%",
+					// make it easy to see
+					textDecorationLine: "none",
+					color: "white",
+				}}
 				data-href={event.path}
 				href={event.path}
 				onMouseUp={() => {
@@ -38,6 +35,10 @@ export default function EventContent({ event }: EventProps<EventDTO>) {
 				parse(event.metadata)
 			) : event.metadata ? (
 				<div
+					style={{
+						padding: "5px",
+						backgroundColor: "var(--background-modifier-form-field)",
+					}}
 					ref={(ref) => ref?.appendChild(event.metadata as Node)}
 					onMouseDown={(e) => {
 						// prioritize event.metadata over react-big-calendar
